19b8235bd0f895d797ccd4e4483ea9e36b655210
itsllyaz <eliyasinbox1@gmail.com>
2026-05-10 11:43:23 +0300

    refactored code and routes

--- main.go
+++ main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/itsllyaz/go-microservices/application"
 )
 
 func main() {
-	router := chi.NewRouter() 
-	router.Use(middleware.Logger)	
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr: ":8080", 
-		Handler: router,
-	}
-	fmt.Print("server running....")
-	err := server.ListenAndServe()
+	app := application.New()		
+
+	err := app.Start(context.TODO())
 	if err != nil{
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello World"))
-}
