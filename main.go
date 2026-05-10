3dda666049ed3e6e3c98665cc898d2654c6eab46
itsllyaz <eliyasinbox1@gmail.com>
2026-05-10 09:50:19 +0300

    chi router & middleware added

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter() 
+	router.Use(middleware.Logger)	
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
 		Addr: ":8080", 
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 	fmt.Print("server running....")
 	err := server.ListenAndServe()
@@ -19,5 +24,5 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request){
-
+	w.Write([]byte("Hello World"))
 }
